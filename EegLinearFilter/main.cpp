ab198af54d3b5fc51e04be2d5fecf13ec716806e
Rastislav Liptak <liptak.rastislav@gmail.com>
2025-11-18 09:01:06 +0100

    Initial Commit

---
+++ EegLinearFilter/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  EegLinearFilter
+//
+//  Created by Rastislav Lipták on 18.11.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
