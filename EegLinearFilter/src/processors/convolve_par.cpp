cf53f458d9ee6b2b3baf5b6191bfdfc917b2e911
Rastislav Liptak <liptak.rastislav@gmail.com>
2025-11-22 18:30:20 +0100

    optimalization of convolve_par_no_vec function

--- EegLinearFilter/src/processors/convolve_par.cpp
+++ EegLinearFilter/src/processors/convolve_par.cpp
@@ -14,8 +14,11 @@
 void convolve_par_no_vec(NeonVector& data, const std::vector<float>& convolutionKernel, const int n) {
     const size_t dataSize = data.size();
     const size_t outSize = dataSize - 2 * n;
+    
+    NeonVector outputBuffer(dataSize);
 
-    float* __restrict dataPtr = data.data();
+    const float* __restrict dataPtr = data.data();
+    float* __restrict outputPtr = outputBuffer.data();
     const float* __restrict kernelPtr = convolutionKernel.data();
 
     dispatch_apply(outSize, dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0), ^(size_t outIndex) {
@@ -27,8 +30,10 @@ void convolve_par_no_vec(NeonVector& data, const std::vector<float>& convolution
         for (int j = -n; j <= n; ++j) {
             sum += dataPtr[i + j] * kernelPtr[j + n];
         }
-        dataPtr[outIndex] = sum;
+        outputPtr[outIndex] = sum;
     });
+    
+    data.swap(outputBuffer);
 }
 
 void convolve_par_no_vec_w_unroll(NeonVector& data, const std::vector<float>& convolutionKernel, const int n) {
